2a36fc1780b4fcca38ffc9888033356060fb2dae
rhydb <hello@rhydb.com>
2022-06-25 21:14:06 +0100

    fix writing and reading. support for other format strings this patch checks for length fields in the format strings. when writing the config, the full format string is then used allowing for support for (most of) the other format string fields.
    however, due to how floats are promoted using sprintf and void* doubles
    MUST use %lf.

--- config.c
+++ config.c
@@ -10,7 +10,6 @@ parseline(char *line, int len, char *key, char *val)
   int si = 0;
   int ki;
   int vi;
-  char quotes = 0;
 
   /* skip spaces until key */
   for (si = 0; si < len && line[si] == ' '; si++);
@@ -32,7 +31,7 @@ parseline(char *line, int len, char *key, char *val)
   }
 
   /* skip until value */
-  for (; si < len && line[si] == ' ' || line[si] == RB_CONFIG_DELIM; si++);
+  for (; (si < len) && (line[si] == ' ' || line[si] == RB_CONFIG_DELIM); si++);
   if (si == len) {
     fprintf(stderr, "key '%s' does not have matching value\n", key);
     return 1;
@@ -53,11 +52,12 @@ parseline(char *line, int len, char *key, char *val)
 void
 rb_readcfg(FILE *in, cfg_t cfg[], size_t n)
 {
-  int i;
+  size_t i;
   size_t len;
   char *line = NULL;
   char key[RB_CONFIG_MAX_KEY_LEN];
   char val[RB_CONFIG_MAX_VAL_LEN];
+  char fmt[6];
 
   while ((len = getline(&line, &len, in)) != -1) {
     if (len > 1) {
@@ -75,7 +75,25 @@ rb_readcfg(FILE *in, cfg_t cfg[], size_t n)
             strcpy(cfg[i].ptr, val);
             break;
           }
-          sscanf(val, cfg[i].fmt, cfg[i].ptr);
+
+          size_t fmt_len = strlen(cfg[i].fmt);
+          char pos = 0;
+          fmt[pos++] = '%';
+
+          if (fmt_len > 2) {
+            if (cfg[i].fmt[fmt_len - 2] == 'h' || cfg[i].fmt[fmt_len - 2] == 'l') {
+              fmt[pos++] = cfg[i].fmt[fmt_len - 2]; // length
+              if (fmt_len > 3) {
+                if (cfg[i].fmt[fmt_len - 3] == cfg[i].fmt[fmt_len - 2]) {
+                  fmt[pos++] = cfg[i].fmt[fmt_len - 3]; // optional second length, e.g '%lld'
+                }
+              }
+            }
+          }
+          fmt[pos++] = cfg[i].fmt[fmt_len - 1]; // type
+          fmt[pos++] = '\0';
+
+          sscanf(val, fmt, cfg[i].ptr);
           break;
         }
       }
@@ -103,10 +121,98 @@ rb_writecfg(FILE *out, cfg_t cfg[], size_t n)
     strcpy(line + offset, " = ");
     offset += 3; // strlen(" = ")
 
-    if (strcmp(cfg[i].fmt, "%s") == 0)
-      strcpy(line+offset, cfg[i].ptr);
-    else
-      sprintf(line + offset, cfg[i].fmt, *((char*)(cfg[i].ptr)));
+#define CAST_TYPE(type) (*((type *)(cfg[i].ptr) ))
+    size_t len = strlen(cfg[i].fmt);
+    char last_char = cfg[i].fmt[len - 1];
+    char data_length = 0;
+    if (len > 2) { // %_x
+      if (cfg[i].fmt[len - 2] == 'l' || cfg[i].fmt[len - 2] == 'h') {
+        data_length = 1;
+        if (len > 3) { // %llx
+          if (cfg[i].fmt[len - 3] == cfg[i].fmt[len - 2]) {
+            data_length = 2;
+          }
+        }
+      }
+    }
+    switch (last_char) {
+      case 's':
+        sprintf(line + offset, cfg[i].fmt, CAST_TYPE(char*));
+        break;
+      case 'c':
+        sprintf(line + offset, cfg[i].fmt, CAST_TYPE(char));
+        break;
+      case 'p':
+        sprintf(line + offset, cfg[i].fmt, CAST_TYPE(void*));
+        break;
+      case 'd':
+      case 'i':
+        if (data_length > 0) {
+          if (cfg[i].fmt[len - 2] == 'l') {
+            if (data_length > 1) {
+              // %lld
+              sprintf(line + offset, cfg[i].fmt, CAST_TYPE(long long));
+            } else {
+              // %ld
+              sprintf(line + offset, cfg[i].fmt, CAST_TYPE(long));
+            }
+          } else  {
+            if (data_length > 1) {
+              // %hhd
+              sprintf(line + offset, cfg[i].fmt, CAST_TYPE(char));
+            } else {
+              // %hd
+              sprintf(line + offset, cfg[i].fmt, CAST_TYPE(short));
+            }
+          }
+        } else {
+          sprintf(line + offset, cfg[i].fmt, CAST_TYPE(int));
+        }
+        break;
+      case 'u':
+      case 'x':
+      case 'X':
+      case 'o':
+        if (data_length > 0) {
+          if (cfg[i].fmt[len - 2] == 'l') {
+            if (data_length > 1) {
+              // %llu
+              sprintf(line + offset, cfg[i].fmt, CAST_TYPE(unsigned long long));
+            } else {
+              // %lu
+              sprintf(line + offset, cfg[i].fmt, CAST_TYPE(unsigned long));
+            }
+          } else  {
+            if (data_length > 1) {
+              // %hhu
+              sprintf(line + offset, cfg[i].fmt, CAST_TYPE(unsigned char));
+            } else {
+              // %hu
+              sprintf(line + offset, cfg[i].fmt, CAST_TYPE(unsigned short));
+            }
+          }
+        } else {
+          sprintf(line + offset, cfg[i].fmt, CAST_TYPE(unsigned int));
+        }
+        sprintf(line + offset, cfg[i].fmt, CAST_TYPE(unsigned int));
+        break;
+      case 'f':
+      case 'F':
+      case 'e':
+      case 'E':
+      case 'g':
+      case 'G':
+      case 'a':
+      case 'A':
+        if (data_length > 0) {
+          sprintf(line + offset, cfg[i].fmt, CAST_TYPE(double));
+        } else {
+          sprintf(line + offset, cfg[i].fmt, CAST_TYPE(float));
+        }
+        break;
+      default:
+        sprintf(line + offset, cfg[i].fmt, CAST_TYPE(char));
+    }
 
     fputs(line, out);
     fputc('\n', out);
